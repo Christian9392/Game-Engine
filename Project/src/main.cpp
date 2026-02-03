1a32fc92a1e6d45577d74dd8a3ab8f024091a42e
Christian <s4005338@student.rmit.edu.au>
2026-02-03 16:19:37 +1100

    feat: Rendered first triangle

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -30,6 +30,12 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 1.0f,  0.5f);
+        glVertex2f( 0.5f, -1.0f);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
